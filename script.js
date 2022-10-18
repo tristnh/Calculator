ed29b13c1489f385062489e6175d43b27784127b
Tristan <tharrisdev@gmail.com>
2022-10-18 14:25:32 -0500

    Add Operate Function This function will take two numbers and an operator and performe that operator on the two numbers

--- script.js
+++ script.js
@@ -18,3 +18,9 @@ const divide = function(num1, num2){
     }
     return num1 / num2;
 }
+
+//Operate Function take two numbers and an operator from above
+
+const operate = function(num1, num2, operator){
+    return operator(num1, num2);
+}
